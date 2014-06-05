927b2a1f8bfebd3afd5e6980858377c7be2d7b97
hannaliebl <hanna@hannaliebl.com>
2014-06-05 13:04:24 -0700

    add second part of problem 1, add first methods to problem 2

--- deque-template.js
+++ deque-template.js
@@ -1,32 +1,44 @@
 //-------
 // Part a): build a deque factory
 function makeDeque(values) {
-	// ...
+    var deque = { dequeValues: [] }
+    for (i = 0; i < values.length; i++) {
+            deque.dequeValues[i] = values[i];
+        }
+    deque.top = makeDeque.top;
+    deque.bottom = makeDeque.bottom;
+    deque.pop = makeDeque.pop;
+    deque.push = makeDeque.push;
+    deque.shift = makeDeque.shift;
+    deque.unshift = makeDeque.unshift;
+    deque.cut = makeDeque.cut;
+    deque.map = makeDeque.map;
+    deque.sort = makeDeque.sort;
+    return deque;
 };
 
 makeDeque.top = function() {
-	//...
+	return this.dequeValues[this.dequeValues.length -1];
 };
 makeDeque.bottom = function() {
-	//...
+	return this.dequeValues[0];
 };
 
 makeDeque.pop = function() {
-	//...
+	return this.dequeValues.pop();
 };
 makeDeque.push = function(val) {
-	//...
+	return this.dequeValues.push(val);
 };
 
 makeDeque.shift = function() {
-	//...
+	return this.dequeValues.shift();
 };
 makeDeque.unshift = function(val) {
-	//...
+	return this.dequeValues.unshift(val);
 };
-
 makeDeque.cut = function(offset) {
-	//...
+	
 };
 
 makeDeque.map = function(convertValFn) {
@@ -36,6 +48,12 @@ makeDeque.map = function(convertValFn) {
 makeDeque.sort = function(compareValsFn) {
 	//...
 };
+
+//sanity checking
+var d2 = makeDeque([1,2,3])
+d2.top();
+d2.bottom();
+d2.pop();
 
 var someCards = /* make array of 52 card objects here, using your code from Problem 1) */;
 // At this point, data looks like Fig.1
@@ -55,4 +73,3 @@ deckOfCards.sort(/* something different here */);
 var someNames = /* make array of student/TA names here */;
 var deckOfNames = makeDeque(someNames);
 deckOfNames.sort(/* something here */);
-
--- problem-1-factory-cards.js
+++ problem-1-factory-cards.js
@@ -33,7 +33,7 @@ makeCard.color = function() {
     var suit = this.suit();
     if (suit < 3) {
         return "red";
-    } else if ((suit > 3) && (suit < 5)) {
+    } else if ((suit > 2) && (suit < 5)) {
         return "black";
     } else {
         return NaN;
@@ -52,6 +52,23 @@ makeCard.precedes = function(cardObj) {
         return (diff == 1) || (diff == -12); 
     }
 }
+makeCard.sameColor = function(cardObj) {
+    var colorA = this.color();
+    var colorB = cardObj.color();
+    if (Number.isNaN(colorA) || Number.isNaN(colorB))
+        return NaN;
+    return colorA == colorB;
+}
+makeCard.nextInSuit = function() {
+    nextCard = this.isValid(this.id, 0, 51) && (this.id)+4;
+    if (nextCard > 51) nextCard -= 52;
+    return nextCard;
+}
+makeCard.prevInSuit = function() {
+    prevCard = this.isValid(this.id, 0, 51) && (this.id)-4;
+    if (prevCard < 0) prevCard += 52;
+    return prevCard;
+}
 
 var card1 = makeCard(1);
 card1.rank();
@@ -61,6 +78,8 @@ card1.color();
 card1.ownName();
 card1.precedes(card2);
 card1.precedes(card3);
+card1.nextInSuit();
+card1.prevInSuit();
 
 var card2 = makeCard("boo");
 card2.rank();
@@ -74,10 +93,15 @@ var card3 = makeCard(51);
 card3.rank();
 card3.suit();
 card3.cardId();
+card3.color();
 card3.ownName();
 card3.precedes(card1);
 
 var card4 = makeCard(30);
 card4.rank();
+card4.suit();
+card4.color();
 card4.precedes(card1);
 card4.precedes(card3);
+card4.sameColor(card1);
+card4.sameColor(card3);
